b71f8a1fe0b79d3f0cf617817251074be6470475
Abdelouahab <abdelouahab.mouici@g.enp.edu.dz>
2026-08-26 20:48:28 +0100

    Update and rename my_main.c to main.c

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 /* USER CODE BEGIN Header */
 /**
-  ****##################################################################################
+  ******************************************************************************
   * @file           : main.c
   * @brief          : Main program body
-  ****##################################################################################
+  ******************************************************************************
   * @attention
   *
   * Copyright (c) 2026 STMicroelectronics.
@@ -13,7 +13,7 @@
   * in the root directory of this software component.
   * If no LICENSE file comes with this software, it is provided AS-IS.
   *
-  ****##################################################################################
+  ******************************************************************************
   */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
@@ -21,8 +21,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include"paso.h"
-#include <math.h>
+#include "paso.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -42,6 +41,7 @@
 
 /* Private variables ---------------------------------------------------------*/
 TIM_HandleTypeDef htim2;
+TIM_HandleTypeDef htim3;
 TIM_HandleTypeDef htim4;
 
 /* USER CODE BEGIN PV */
@@ -49,13 +49,18 @@ stepper s1;
 stepper s2;
 robot myRobot;
 volatile uint8_t obstacle = 0;
-float distance=0 ;
+volatile uint8_t turn_right = 0;
+float distance = 0;
+volatile uint8_t start = 0;
+volatile uint8_t lastState = GPIO_PIN_RESET;
+volatile uint8_t currentState;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_TIM2_Init(void);
+static void MX_TIM3_Init(void);
 static void MX_TIM4_Init(void);
 /* USER CODE BEGIN PFP */
 
@@ -72,94 +77,83 @@ static void MX_TIM4_Init(void);
   */
 int main(void)
 {
-
   /* USER CODE BEGIN 1 */
-
-	// Initialization of parameters
-
-	  s1.htim       = &htim2;
-	  s1.dir_port   = GPIOA;
-	  s1.dir_pin    = GPIO_PIN_1;
-	  s1.step_port  = GPIOA;
-	  s1.step_pin   = GPIO_PIN_2;
-	  s1.speed_rad_s = 10.0f;
-
-	  s1.trig_port  = GPIOB;
-	  s1.trig_pin   = GPIO_PIN_4;
-	  s1.echo_port  = GPIOB;
-	  s1.echo_pin   = GPIO_PIN_6;
-
-	  s2.htim       = &htim2;
-	  s2.dir_port   = GPIOA;
-	  s2.dir_pin    = GPIO_PIN_3;
-	  s2.step_port  = GPIOA;
-	  s2.step_pin   = GPIO_PIN_4;
-	  s2.speed_rad_s = 10.0f;
-
-	  myRobot.left         = &s1;
-	  myRobot.right        = &s2;
-	  myRobot.wheel_radius = 0.032f; // 3.2 cm
-	  myRobot.wheel_base   = 0.102f; // 10.7 cm
 
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
 
   /* USER CODE BEGIN Init */
 
   /* USER CODE END Init */
 
-  /* Configure the system clock */
   SystemClock_Config();
 
   /* USER CODE BEGIN SysInit */
 
   /* USER CODE END SysInit */
 
-  /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_TIM2_Init();
+  MX_TIM3_Init();
   MX_TIM4_Init();
   /* USER CODE BEGIN 2 */
 
+  // Hardware mapping
+  s1.htim        = &htim2;
+  s1.dir_port    = GPIOA; s1.dir_pin  = GPIO_PIN_1;
+  s1.step_port   = GPIOA; s1.step_pin = GPIO_PIN_2;
+  s1.speed_rad_s = 15.0f;
+
+  s1.trig_port  = GPIOB;
+  s1.trig_pin   = GPIO_PIN_4;
+  s1.echo_port  = GPIOB;
+  s1.echo_pin   = GPIO_PIN_6;
+
+  s2.htim        = &htim2;
+  s2.dir_port    = GPIOA; s2.dir_pin  = GPIO_PIN_4;
+  s2.step_port   = GPIOA; s2.step_pin = GPIO_PIN_5;
+  s2.speed_rad_s = 15.0f;
+
+  myRobot.left         = &s1;
+  myRobot.right        = &s2;
+  myRobot.wheel_radius = 0.032f;
+  myRobot.wheel_base   = 0.128f;
+
   HAL_TIM_IC_Start_IT(&htim4, TIM_CHANNEL_1);
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_3);
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_4);
 
-  // Quater step activation
+  uint8_t limit_switch_pressed = 0;
+  uint8_t last_switch_state = 0;
+  uint8_t start = 0;
 
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_SET);
+  while (start == 0)
+  {
+      limit_switch_pressed = (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_7) == GPIO_PIN_SET) ? 1 : 0;
 
+      // Detect FALLING edge (pressed -> released)
+      if (limit_switch_pressed == 0 && last_switch_state == 1)
+      {
+          start = 1;  // Trigger on release
+      }
 
+      last_switch_state = limit_switch_pressed;
 
+      HAL_Delay(10);
+  }
 
-  // Start Moving following the path 
+  // --- Read direction switch PB8 once ---
+  if (HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_8) == GPIO_PIN_RESET) {
+      turn_right = 1;  // connected to GND = turn right
+  } else {
+      turn_right = 0;  // floating = turn left
+  }
 
-    uint32_t steps_1m = distance_to_steps(1.0f, myRobot.wheel_radius);
-
-    move_two_steppers(&s1, &s2, steps_1m, GPIO_PIN_RESET);
-    while(s1.steps_left > 0 || s2.steps_left > 0) {
-        sensor_control_loop(&s1);
-    }
-
-    HAL_Delay(500);
-
-
-    robot_rotate(&myRobot, 180.0f, 1);
-    while(s1.steps_left > 0 || s2.steps_left > 0) {
-        sensor_control_loop(&s1);
-    }
-
-    HAL_Delay(500);
-
-
-    move_two_steppers(&s1, &s2, steps_1m, GPIO_PIN_RESET);
-    while(s1.steps_left > 0 || s2.steps_left > 0) {
-        sensor_control_loop(&s1);
-    }
-
+  // --- Pick and run one of the 4 predefined trajectories ---
+  uint8_t traj_index = turn_right ? 0 : 1; // TODO: adjust selection logic as needed
+  robot_execute_trajectory(&myRobot, &trajectories[traj_index]);
 
   /* USER CODE END 2 */
 
@@ -167,12 +161,16 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+    for (int pulse = 1000; pulse <= 2000; pulse += 5)
+    {
+        __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_3, pulse);
+        __HAL_TIM_SET_COMPARE(&htim3, TIM_CHANNEL_4, pulse);
+        HAL_Delay(10);
+    }
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
   }
-
-
   /* USER CODE END 3 */
 }
 
@@ -185,14 +183,9 @@ void SystemClock_Config(void)
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-  /** Configure the main internal regulator output voltage
-  */
   __HAL_RCC_PWR_CLK_ENABLE();
   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
@@ -207,8 +200,6 @@ void SystemClock_Config(void)
     Error_Handler();
   }
 
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
@@ -224,22 +215,12 @@ void SystemClock_Config(void)
 
 /**
   * @brief TIM2 Initialization Function
-  * @param None
-  * @retval None
   */
 static void MX_TIM2_Init(void)
 {
-
-  /* USER CODE BEGIN TIM2_Init 0 */
-
-  /* USER CODE END TIM2_Init 0 */
-
   TIM_ClockConfigTypeDef sClockSourceConfig = {0};
   TIM_MasterConfigTypeDef sMasterConfig = {0};
 
-  /* USER CODE BEGIN TIM2_Init 1 */
-
-  /* USER CODE END TIM2_Init 1 */
   htim2.Instance = TIM2;
   htim2.Init.Prescaler = 83;
   htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
@@ -261,31 +242,67 @@ static void MX_TIM2_Init(void)
   {
     Error_Handler();
   }
-  /* USER CODE BEGIN TIM2_Init 2 */
+}
 
-  /* USER CODE END TIM2_Init 2 */
+/**
+  * @brief TIM3 Initialization Function
+  */
+static void MX_TIM3_Init(void)
+{
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_OC_InitTypeDef sConfigOC = {0};
 
+  htim3.Instance = TIM3;
+  htim3.Init.Prescaler = 83;
+  htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim3.Init.Period = 19999;
+  htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim3, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_PWM_Init(&htim3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigOC.OCMode = TIM_OCMODE_PWM1;
+  sConfigOC.Pulse = 1500;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  if (HAL_TIM_PWM_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigOC.Pulse = 0;
+  if (HAL_TIM_PWM_ConfigChannel(&htim3, &sConfigOC, TIM_CHANNEL_3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  HAL_TIM_MspPostInit(&htim3);
 }
 
 /**
   * @brief TIM4 Initialization Function
-  * @param None
-  * @retval None
   */
 static void MX_TIM4_Init(void)
 {
-
-  /* USER CODE BEGIN TIM4_Init 0 */
-
-  /* USER CODE END TIM4_Init 0 */
-
   TIM_ClockConfigTypeDef sClockSourceConfig = {0};
   TIM_MasterConfigTypeDef sMasterConfig = {0};
   TIM_IC_InitTypeDef sConfigIC = {0};
 
-  /* USER CODE BEGIN TIM4_Init 1 */
-
-  /* USER CODE END TIM4_Init 1 */
   htim4.Instance = TIM4;
   htim4.Init.Prescaler = 83;
   htim4.Init.CounterMode = TIM_COUNTERMODE_UP;
@@ -314,111 +331,78 @@ static void MX_TIM4_Init(void)
   sConfigIC.ICPolarity = TIM_INPUTCHANNELPOLARITY_RISING;
   sConfigIC.ICSelection = TIM_ICSELECTION_DIRECTTI;
   sConfigIC.ICPrescaler = TIM_ICPSC_DIV1;
-  sConfigIC.ICFilter = 4;
+  sConfigIC.ICFilter = 0;
   if (HAL_TIM_IC_ConfigChannel(&htim4, &sConfigIC, TIM_CHANNEL_1) != HAL_OK)
   {
     Error_Handler();
   }
-  /* USER CODE BEGIN TIM4_Init 2 */
-
-  /* USER CODE END TIM4_Init 2 */
-
 }
 
 /**
   * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
   */
 static void MX_GPIO_Init(void)
 {
   GPIO_InitTypeDef GPIO_InitStruct = {0};
-  /* USER CODE BEGIN MX_GPIO_Init_1 */
 
-  /* USER CODE END MX_GPIO_Init_1 */
-
-  /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOH_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4
-                          |GPIO_PIN_5|GPIO_PIN_6, GPIO_PIN_RESET);
-
-  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_4|GPIO_PIN_5, GPIO_PIN_RESET);
   HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, GPIO_PIN_RESET);
 
-  /*Configure GPIO pins : PA1 PA2 PA3 PA4
-                           PA5 PA6 */
-  GPIO_InitStruct.Pin = GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4
-                          |GPIO_PIN_5|GPIO_PIN_6;
+  GPIO_InitStruct.Pin = GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_4|GPIO_PIN_5;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-  /*Configure GPIO pin : PB4 */
   GPIO_InitStruct.Pin = GPIO_PIN_4;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-  /* USER CODE BEGIN MX_GPIO_Init_2 */
+  GPIO_InitStruct.Pin = GPIO_PIN_7;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-  /* USER CODE END MX_GPIO_Init_2 */
+  GPIO_InitStruct.Pin = GPIO_PIN_8;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 }
 
 /* USER CODE BEGIN 4 */
-
-
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 {
-    if (htim == &htim2)
+    if (htim->Instance == TIM2)
     {
-    	isr_stepper_all(&s1, &s2);
+        isr_stepper_all(&s1, &s2);
     }
 }
 
 void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim)
 {
-	if (htim->Instance == TIM4) {
-		isr_ultrasonic(htim);
-	}
-
+    if (htim->Instance == TIM4)
+    {
+        isr_ultrasonic(htim);
+    }
 }
-
-
 /* USER CODE END 4 */
 
-/**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
 void Error_Handler(void)
 {
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
   __disable_irq();
   while (1)
   {
   }
-  /* USER CODE END Error_Handler_Debug */
 }
-#ifdef USE_FULL_ASSERT
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+
+#ifdef  USE_FULL_ASSERT
 void assert_failed(uint8_t *file, uint32_t line)
 {
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
